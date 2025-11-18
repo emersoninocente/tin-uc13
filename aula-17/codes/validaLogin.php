407bb08ad3fd1bc8fa2bdc4a9802560e62ed9ebf
Emerson Meneses Inocente <35742093+emersoninocente@users.noreply.github.com>
2025-11-18 10:01:57 -0300

    Add login validation in validaLogin.php
    Validate email and password input before redirecting.

---
+++ aula-17/codes/validaLogin.php
@@ -0,0 +1,16 @@
+<?php
+if (empty($_POST['e-mail'])) {
+    header('Location: login.php?erro=validacao');
+    exit();
+}
+
+if (empty($_POST['pass'])) {
+    header('Location: login.php?erro=validacao');
+    exit();
+}
+
+// Falta validar user e pass no banco já visto em aula
+session_start();
+$_SESSION['user'] = $_POST['e-mail'];
+header('Location: src/Views/listarUsuarios.php');
+?>
